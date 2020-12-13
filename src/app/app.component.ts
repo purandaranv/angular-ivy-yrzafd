a34c26d34e135f75cb35375a04550c1794bff989
purandaranv <purandaranv@gmail.com>
2020-12-13 09:48:51 +0530

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,38 @@
-import { Component } from '@angular/core';
-import { freeApiService } from './services/freeapi.service';
-import { Comments} from './classes/comments';
-import { Posts} from './classes/posts';
-
+import { Component } from "@angular/core";
+import { freeApiService } from "./services/freeapi.service";
+import { Comments } from "./classes/comments";
+import { Posts } from "./classes/posts";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  constructor(private _freeApiSerives: freeApiService){ }
+  constructor(private _freeApiSerives: freeApiService) {}
 
   listcomments: Comments[];
   listPosts: Posts[];
   objPosts: Posts;
 
-  title = 'angular10callrestapi';
-  
-  ngOnInit() {
-    this._freeApiSerives.getcomments()
-    .subscribe(
-      data=>{
-        this.listcomments = data;
-      }
-    );
+  title = "angular10callrestapi";
 
-    this._freeApiSerives.getcommentsbyparameter()
-    .subscribe(
-      data=>{
+  ngOnInit() {
+    this._freeApiSerives.getcomments().subscribe(data => {
+      this.listcomments = data;
+    });
+
+    this._freeApiSerives.getcommentsbyparameter().subscribe(data => {
       this.listPosts = data;
     });
 
     var opost = new Posts();
-    opost.body="testbody";
-    opost.title="testtitle";
-    opost.userId=5;
+    opost.body = "testbody";
+    opost.title = "testtitle";
+    opost.userId = 5;
 
-    this._freeApiSerives.post(opost)
-    .subscribe(
-      data=>{
-        this.objPosts = data;
-      }
-    );
+    this._freeApiSerives.post(opost).subscribe(data => {
+      this.objPosts = data;
+    });
   }
 }
